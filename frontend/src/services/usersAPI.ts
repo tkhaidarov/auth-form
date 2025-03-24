843b6e24051117f55946458fbd6490e68d430bce
tkhaidarov <168107546+tkhaidarov@users.noreply.github.com>
2025-03-24 16:28:32 +0500

    url is updated

--- frontend/src/services/usersAPI.ts
+++ frontend/src/services/usersAPI.ts
@@ -4,7 +4,7 @@ import { IUserProfile } from '../types/types';
 export const usersAPI = createApi({
   reducerPath: 'usersApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: `${import.meta.env.VITE_API_URL}/api/auth`,
+    baseUrl: import.meta.env.VITE_API_URL,
     prepareHeaders: headers => {
       const token = localStorage.getItem('token') || '';
       if (token) {
