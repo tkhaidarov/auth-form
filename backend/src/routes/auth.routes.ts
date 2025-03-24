6bf420aebb54b3e1b7192617315d854353757b40
tkhaidarov <168107546+tkhaidarov@users.noreply.github.com>
2025-03-24 17:14:53 +0500

    url is updated

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,13 +1,13 @@
 import { Router, Request, Response } from 'express';
 import { register } from '../controllers/auth.controller';
 import { login } from '../controllers/login.controller';
-import { authenticate } from '../middleware/middleware';
-import { IAuthRequest } from '../types/IAuthRequest';
+// import { authenticate } from '../middleware/middleware';
+// import { IAuthRequest } from '../types/IAuthRequest';
 
 const router = Router();
 router.post('/register', register);
 router.post('/login', login);
-router.get('/profile', authenticate, (req: IAuthRequest, res: Response) => {
-  res.json({ message: 'Welcome to your profile!', userId: req.user?.id });
-});
+// router.get('/profile', authenticate, (req: IAuthRequest, res: Response) => {
+//   res.json({ message: 'Welcome to your profile!', userId: req.user?.id });
+// });
 export default router;
